d3e536f33b31b0016ac98274d503d9f9522c7cb2
Serhii Kobzar <serhii_kobzar@epam.com>
2016-09-22 12:36:35 +0300

    qwe

--- GitHomework/EpamEmployee/src/com/company/Direction.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-/**
- * Created by Serhii_Kobzar on 9/19/2016.
- */
-public enum Direction {
-    TA, Manual, DevOps, JavaDev, Front_EndDev
-}
--- GitHomework/Task7/src/com/company/Group.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-/**
- * Created by Serhii_Kobzar on 9/20/2016.
- */
-public enum Group {
-    RED,BLUE,GREEN
-}
